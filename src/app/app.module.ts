30f0eefa9eb4d3636bef4d2097e9a7a9241656d0
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-11-21 20:02:22 -0300

    Adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { MenuComponent } from './components/menu/menu.component';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './components/post/post.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    NewPostComponent,
+    PostComponent,
+    ProfileComponent
   ],
+  providers: [],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule { }
